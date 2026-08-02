8c8b244e4a1cb4a26fcaa70f7e2b12b0ba6990cb
Radha Gopinath <radhagopinath@mac.lan>
2026-08-02 21:23:07 +0100

    Restore official TradingView JS library widget for built-in view to support all symbols (including ETERNAL, BEL) without limitations

--- dashboard.py
+++ dashboard.py
@@ -209,7 +209,7 @@ th.sort-on.asc::after{content:' ▲'}
       Select a stock row or alert from the list to load the TradingView chart
     </div>
     <div id="tv-widget-container" style="flex:1;width:100%;height:100%;display:none;overflow:hidden;background:#151924;position:relative">
-      <iframe id="tv-widget-built-in" style="width:100%;height:100%;position:absolute;top:0;left:0;border:none"></iframe>
+      <div id="tv-widget-built-in" style="width:100%;height:100%;position:absolute;top:0;left:0"></div>
       <iframe id="tv-widget-premium" style="width:100%;height:100%;display:none;border:none;position:absolute;top:0;left:0"></iframe>
     </div>
   </div>
@@ -268,12 +268,30 @@ function loadTVChart(symbol, tf) {
   } else {
     premiumIframe.style.display = 'none';
     builtInDiv.style.display = 'block';
+    builtInDiv.innerHTML = '';
     
     let tvInterval = "1";
     if (tf === 5) tvInterval = "5";
     if (tf === 15) tvInterval = "15";
     
-    builtInDiv.src = `https://s.tradingview.com/widgetembed/?symbol=NSE:${symbol}&interval=${tvInterval}&theme=dark&style=1&timezone=Asia/Kolkata&studies=RSI%40tv-basicstudies%3BMACD%40tv-basicstudies%3BMAExp%40tv-basicstudies`;
+    new TradingView.widget({
+      "autosize": true,
+      "symbol": "NSE:" + symbol,
+      "interval": tvInterval,
+      "timezone": "Asia/Kolkata",
+      "theme": "dark",
+      "style": "1",
+      "locale": "en",
+      "enable_publishing": false,
+      "hide_side_toolbar": false,
+      "allow_symbol_change": true,
+      "container_id": "tv-widget-built-in",
+      "studies": [
+        "RSI@tv-basicstudies",
+        "MACD@tv-basicstudies",
+        "MAExp@tv-basicstudies"
+      ]
+    });
   }
 }
 
